9538aeda3771185d94315ad6ebb7c23b71a757ab
Sebastian Bengtegård <sebastianbengtegard@gmail.com>
2021-01-22 09:40:53 +0100

    testing eslint

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("1");
+
+const a = 1;
+
+for (let i = 0; i<5; i++) {
+console.log(i);
+}
+
+console.log(a, "hehe");
+
+if(a==1)
+{
+console.log(1);
+    }
+
+let arr = [2, 2, 3];
+let arr2 = [1, 3, 3];
+
+console.log(arr, arr2);
